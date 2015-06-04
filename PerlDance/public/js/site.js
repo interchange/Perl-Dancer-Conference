1b1a6132d9aabce5258c949c72503c8108c46984
Peter Mottram <peter@sysnix.com>
2015-06-04 19:02:10 +0200

    implement countdown counter in pure js
    original site sets values in html and uses those for countdown but now
    this is done purely in js.

--- PerlDance/public/js/site.js
+++ PerlDance/public/js/site.js
@@ -1,7 +1,62 @@
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 150) {
-    $('.navbar').addClass('shrink');
-  }
-  else {
-    $('.navbar').removeClass('shrink'); }
+$(document).ready(function() {
+  /* shrink navbar */
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 150) {
+      $('.navbar').addClass('shrink');
+    }
+    else {
+      $('.navbar').removeClass('shrink'); }
+  });
+  $('.gdlr-session-counter-item').each(function(){
+      this.finalDate = new Date("2015-10-19T09:00:00+01:00");
+      this.now = new Date();
+      this.totalSecsLeft = (this.finalDate.getTime() - this.now.getTime())/1000;
+
+      var day_field = $(this).find('.gdlr-day');
+      var day = Math.floor(this.totalSecsLeft / 60 / 60 / 24);
+      day_field.text(day);
+      
+      var hrs_field = $(this).find('.gdlr-hrs');
+      var hrs = Math.floor(this.totalSecsLeft / 60 / 60) % 24;
+      hrs_field.text(hrs);
+      
+      var min_field = $(this).find('.gdlr-min');
+      var min = Math.floor(this.totalSecsLeft / 60) % 60;
+      min_field.text(min);
+      
+      var sec_field = $(this).find('.gdlr-sec');
+      var sec = Math.floor(this.totalSecsLeft) % 60;
+      sec_field.text(sec);    
+      
+      var i = setInterval(function(){
+          if( sec > 0 ){
+              sec--;          
+          }else{
+              sec = 59;
+              if( min > 0 ){
+                  min--;
+              }else{
+                  min = 59;
+                  if( hrs > 0 ){
+                      hrs--;
+                  }else{
+                      hrs = 24;
+                      if( day > 0 ){
+                          day--;
+                      }else{
+                          day = 0;
+                          hrs = 0;
+                          min = 0;
+                          sec = 0;
+                          clearInterval(i);
+                      }
+                      day_field.text(day);
+                  }
+                  hrs_field.text(hrs);
+              }
+              min_field.text(min);
+          }
+          sec_field.text(sec);    
+      }, 1000);
+  });
 });
