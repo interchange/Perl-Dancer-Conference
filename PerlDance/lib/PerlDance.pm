54afc7cb284d45c0c2762f8391785fc86feec324
Peter Mottram <peter@sysnix.com>
2015-06-08 13:53:46 +0200

    add speakers fragment and include it in / and /speakers

--- PerlDance/lib/PerlDance.pm
+++ PerlDance/lib/PerlDance.pm
@@ -1,19 +1,54 @@
 package PerlDance;
 use Dancer ':syntax';
+use Dancer::Plugin::Auth::Extensible;
+use Dancer::Plugin::DBIC;
+use Dancer::Plugin::Interchange6;
 
 our $VERSION = '0.1';
 
+set session => 'DBIC';
+set session_options => { schema => schema };
+
 get '/' => sub {
-    template 'index';
+    my $tokens = {};
+
+    add_speakers_tokens($tokens);
+
+    template 'index', $tokens;
 };
 
 get '/speakers' => sub {
-    template 'speakers';
+    my $tokens = {};
+
+    add_speakers_tokens($tokens);
+
+    template 'speakers', $tokens;
 };
 
-get '/speakers/:speaker' => sub {
-    my $speaker = param 'speaker';
+get '/speakers/:id' => sub {
+    my $users_id = param 'id';
     template 'speaker_detail';
 };
+
+sub add_speakers_tokens {
+    my $tokens = shift;
+
+    my @speakers = shop_user->search(
+        {
+            'addresses.type' => 'primary',
+        },
+        {
+            prefetch => [ { addresses => 'country', }, 'photo' ],
+            join => { photo => 'displays' }
+        }
+    )->all;
+
+    my @grid;
+    while ( my @row = splice( @speakers, 0, 4 ) ) {
+        push @grid, +{ row => \@row };
+    }
+
+    $tokens->{speakers} = \@grid;
+}
 
 true;
