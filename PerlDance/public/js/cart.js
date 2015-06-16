0fc5a2bf7cf13c1f0b16d47881d4529515e2d0f0
Peter Mottram <peter@sysnix.com>
2015-06-16 12:54:13 +0200

    more work on cart update handling

--- PerlDance/public/js/cart.js
+++ PerlDance/public/js/cart.js
@@ -1,24 +1,37 @@
+// cart product quantity change
+$(".cart-container").on( "change", ".product .quantity", function() {
+    var el = $(this);
+    var qty = el.val();
+    var sku = el.closest(".product").find("input.sku").val();
+    if ( qty === '10+' ) {
+        // switch from select to input field
+
+    }
+    else {
+        $.ajax({
+            type: "POST",
+            url: "/cart",
+            data: { update: sku, quantity: qty },
+            success: function(json) {
+                el.closest("table").replaceWith(json.html);
+            }
+        });
+    }
+});
+// delete product from cart
+$(".cart-container").on( "click", ".product .remove-sku", function() {
+    var el = $(this);
+    $.ajax({
+        type: "GET",
+        url: el.attr("href"),
+        success: function(json) {
+            el.closest("table").replaceWith(json.html);
+        }
+    });
+    return false;
+});
 $(document).ready(function() {
     $(".product .quantity").each(function() {
         $(this).data("oldValue", $(this).val());
-    });
-    $(document).on( "change", ".product .quantity", function() {
-        var el = $(this);
-        var qty = el.val();
-        var sku = el.closest(".product").find("input.sku").val();
-        if ( qty === '10+' ) {
-            // switch from select to input field
-
-        }
-        else {
-            $.ajax({
-                type: "POST",
-                url: "/cart",
-                data: { update: sku, quantity: qty },
-                success: function(json) {
-                    el.closest("table").replaceWith(json.html);
-                }
-            });
-        }
     });
 });
