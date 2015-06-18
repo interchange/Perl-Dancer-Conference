2ae1b6eba32e2cfe78e24d1eb042762b05e4e936
Peter Mottram <peter@sysnix.com>
2015-06-18 15:14:02 +0200

    split routes out into separate modules closes #6

---
+++ PerlDance/lib/PerlDance/Routes.pm
@@ -0,0 +1,277 @@
+package PerlDance::Routes;
+
+=head1 NAME
+
+PerlDance::Routes - routes for PerlDance conference application
+
+=cut
+
+use Dancer ':syntax';
+use Dancer::Plugin::Auth::Extensible;
+use Dancer::Plugin::Interchange6;
+use Dancer::Plugin::Interchange6::Routes;
+use Try::Tiny;
+
+use PerlDance::Routes::Account;
+
+=head1 ROUTES
+
+See also: L<PerlDance::Routes::Account>
+
+=head2 get /
+
+Home page
+
+=cut
+
+get '/' => sub {
+    my $tokens = {};
+
+    add_speakers_tokens($tokens);
+
+    my $nav = shop_navigation->find( { uri => 'tickets' } );
+    $tokens->{tickets} = [ $nav->products->active->hri->all ];
+
+    $tokens->{title} = "Vienna Austria October 2015";
+
+    add_javascript( $tokens, "//maps.google.com/maps/api/js?sensor=false",
+        "/js/index.js" );
+
+    template 'index', $tokens;
+};
+
+=head2 get /speakers
+
+Speaker list
+
+=cut
+
+get '/speakers' => sub {
+    my $tokens = {};
+
+    my $nav = shop_navigation->find( { uri => 'speakers' } );
+    $tokens->{title}       = $nav->name;
+    $tokens->{description} = $nav->description;
+
+    add_speakers_tokens($tokens);
+
+    $tokens->{title} = "Speakers";
+
+    template 'speakers', $tokens;
+};
+
+=head2 get /speakers/{id}.*
+
+Individual speaker
+
+=cut
+
+get qr{/speakers/(?<id>\d+).*} => sub {
+    my $users_id = captures->{id};
+    my $tokens = {};
+
+    $tokens->{user} = shop_user->search(
+        {
+            'me.users_id'    => $users_id,
+            'addresses.type' => 'primary',
+        },
+        {
+            prefetch =>
+              [ { addresses => 'country', }, 'photo', 'talks_authored' ],
+        }
+    )->first;
+
+    if ( !$tokens->{user} ) {
+        status 'not_found';
+        return "Speaker not found";
+    }
+
+    $tokens->{has_talks} = 1 if $tokens->{user}->talks_authored->has_rows;
+
+    $tokens->{title} = $tokens->{user}->name;
+
+    template 'speaker', $tokens;
+};
+
+=head2 get /talks
+
+Talks list
+
+=cut
+
+get '/talks' => sub {
+    my $tokens = {};
+
+    my $nav = shop_navigation->find( { uri => 'talks' } );
+    $tokens->{title}       = $nav->name;
+    $tokens->{description} = $nav->description;
+
+    my $talks = shop_schema->resultset('Talk')->search(
+        {
+            accepted => 1,
+        },
+        {
+            order_by => 'start_time',
+            prefetch => 'author',
+        }
+    );
+    my %days;
+    while ( my $talk = $talks->next ) {
+        my $day_zero = 18;
+        my $day_number = 0;
+        if ( defined $talk->start_time ) {
+            $day_number = $talk->start_time->day - $day_zero;
+        }
+        push @{ $days{$day_number} }, $talk;
+    };
+    my $unscheduled;
+    foreach my $day ( sort keys %days ) {
+        my $value = $days{$day};
+        my $date = $value->[0]->start_time;
+        if ( $day > 0 ) {
+            push @{ $tokens->{talks} },
+              {
+                day   => "Day $day",
+                date  => $date,
+                talks => $value,
+              };
+        }
+        else {
+            $unscheduled = {
+                day   => 'Not yet scheduled',
+                date  => undef,
+                talks => $value,
+            };
+        }
+    }
+    push @{ $tokens->{talks} }, $unscheduled if $unscheduled;
+
+    template 'talks', $tokens;
+};
+
+=head2 get /talks/submit
+
+CFP
+
+=cut
+
+get '/talks/submit' => sub {
+    my $tokens = {};
+
+    $tokens->{title} = "Call For Papers";
+
+    template 'cfp', $tokens;
+};
+
+=head2 get /talks/{id}.*
+
+Individual talk
+
+=cut
+
+get qr{/talks/(?<id>\d+).*} => sub {
+    my $talks_id = captures->{id};
+    my $tokens = {};
+
+    $tokens->{talk} = shop_schema->resultset('Talk')->search(
+        {
+            'me.talks_id' => $talks_id,
+        },
+        {
+            prefetch => [ 'author', { attendee_talks => 'user' } ],
+        }
+    )->first;
+
+    $tokens->{title} = $tokens->{talk}->title;
+
+    template 'talk', $tokens;
+};
+
+=head2 get /tickets
+
+Conference tickets
+
+=cut
+
+get '/tickets' => sub {
+    my $tokens = {};
+
+    my $nav = shop_navigation->find( { uri => 'tickets' } );
+    $tokens->{title}       = $nav->name;
+    $tokens->{description} = $nav->description;
+    $tokens->{tickets}     = [$nav->products->active->hri->all];
+
+    template 'tickets', $tokens;
+};
+
+=head2 shop_setup_routes
+
+L<Dancer::Plugin::Interchange6::Routes/shop_setup_routes>
+
+=cut
+
+shop_setup_routes;
+
+=head2 not_found
+
+404
+
+=cut
+
+any qr{.*} => sub {
+    my $tokens = {};
+
+    $tokens->{title} = "Not Found";
+
+    status 'not_found';
+    template '404', $tokens;
+};
+
+=head1 METHODS
+
+=head2 add_javascript($tokens, @js_urls);
+
+=cut
+
+sub add_javascript {
+    my $tokens = shift;
+    foreach my $src ( @_ ) {
+        push @{ $tokens->{"extra-js"} }, { src => $src };
+    }
+}
+
+=head2 add_speakers_tokens($tokens);
+
+Add tokens needed by speakers fragment
+
+=cut
+
+sub add_speakers_tokens {
+    my $tokens = shift;
+
+    my @speakers = shop_user->search(
+        {
+            'addresses.type' => 'primary',
+            -or              => {
+                'talks_authored.accepted' => 1,
+                -and => {
+                    'attribute.name' => 'speaker',
+                    'attribute.type' => 'boolean',
+                },
+            },
+        },
+        {
+            prefetch => [ { addresses => 'country', }, 'photo' ],
+            join => [ 'talks_authored', { user_attributes => 'attribute' } ],
+        }
+    )->all;
+
+    my @grid;
+    while ( my @row = splice( @speakers, 0, 4 ) ) {
+        push @grid, +{ row => \@row };
+    }
+
+    $tokens->{speakers} = \@grid;
+}
+
+true;
---
+++ PerlDance/lib/PerlDance/Routes/Account.pm
@@ -0,0 +1,102 @@
+package PerlDance::Routes::Account;
+
+=head1 NAME
+
+PerlDance::Routes::Account - account routes such as login, edit profile, ...
+
+=cut
+
+use Dancer ':syntax';
+use Dancer::Plugin::Auth::Extensible;
+use Dancer::Plugin::Email;
+use Dancer::Plugin::Interchange6;
+use HTML::FormatText::WithLinks;
+use Try::Tiny;
+
+=head1 ROUTES 
+
+=head2 get /login
+
+post is handled by L<Dancer::Plugin::Interchange6>
+
+=cut
+
+get '/login' => sub {
+    my $nav = shop_navigation->find( { uri => 'login' } );
+    my $tokens = {
+        title       => $nav->name,
+        description => $nav->description,
+    };
+    $tokens->{title} = "Login";
+    template 'login', $tokens;
+};
+
+=head2 post /register
+
+Register of request password reset.
+
+=cut
+
+post '/register' => sub {
+    my $username = param 'username';
+    # TODO: validate
+
+    debug "register/reset for username: $username";
+
+    my $user = shop_user( { username => $username } );
+    if ($user) {
+
+        # password reset
+    }
+    else {
+
+        # new registration
+        try {
+            $user = shop_user->create(
+                { username => $username, email => $username } );
+        }
+        catch {
+            error "registration failed: $_";
+            # TODO: send email to admins as well?
+        }
+    }
+
+    if ( $user ) {
+        my $token = $user->reset_token_generate;
+
+        my $html = template "email/generic",
+          {
+            "conference-logo" => uri_for(
+                shop_schema->resultset('Media')
+                  ->search( { label => "email-logo" } )->first->uri
+            ),
+            preamble => "You are receiving this email because your email address was used to register for the Perl Dancer Conference 2015.\n\nIf you received this email in error please accept our apologies and delete this email. No further action is required on your part.\n\nTo continue with registration please click on the following link:",
+            link => uri_for("/register/confirm/$token")
+          },
+          { layout => 'email' };
+
+        my $f = HTML::FormatText::WithLinks->new;
+        my $text = $f->parse($html);
+        try {
+            email {
+                to      => $user->email,
+                subject => "Registration for the Perl Dancer Conference 2015",
+                body    => $text,
+                type    => 'text',
+                attach  => {
+                    Data     => $html,
+                    Encoding => "quoted-printable",
+                    Type     => "text/html"
+                },
+                multipart => 'alternative',
+            };
+        }
+        catch {
+            error "Could not send email: $_";
+        }
+    }
+
+    template 'register', { username => $username };
+};
+
+true;
