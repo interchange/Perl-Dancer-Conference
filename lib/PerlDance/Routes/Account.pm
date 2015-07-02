ad08b55e7ec7a3cb3abe8ccd90aadcdecbaa9213
Peter Mottram <peter@sysnix.com>
2015-07-02 16:51:25 +0200

    more progress on profile photo upload/edit

--- lib/PerlDance/Routes/Account.pm
+++ lib/PerlDance/Routes/Account.pm
@@ -74,6 +74,9 @@ get '/profile/photo' => require_login sub {
 };
 
 post '/profile/photo' => require_login sub {
+    my $file = upload('photo');
+    debug "got file ********* " if $file;
+    debug keys params;
     content_type('application/json');
     to_json({ data => 'foo' });
 };
--- public/js/profile-photo.js
+++ public/js/profile-photo.js
@@ -1,6 +1,19 @@
-$(document).ready(function() {
-    var xsrfMeta = $('head meta[name="xsrf-meta"]').attr('content');
+$(document).on('ready', function(){
     $("#photo-upload").fileinput({
-        uploadExtraData: { xsrf_token: xsrfMeta }
+        uploadExtraData: { xsrf_token:
+            $('head meta[name="xsrf-meta"]').attr('content') },
+        uploadUrl: '/profile/photo',
+        uploadAsync: true,
+        maxFileCount: 1,
+        allowedFileTypes: ['image'],
+        allowedFileExtensions: ['jpg', 'gif', 'png'],
+        minImageHeight: 300,
+        minImageWidth: 300,
+        maxFileSize: 1024
+    });
+    $("#photo-upload").on('fileuploaded', function(e, data) {
+        $(".upload-panel").addClass('hidden');
+        $(".crop-panel").removeClass('hidden');
     });
 });
+
