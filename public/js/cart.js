1562519b156132ac86b018a767b768ab4d969cae
Peter Mottram <peter@sysnix.com>
2015-07-22 17:11:35 +0200

    small cart JS fix

--- public/js/cart.js
+++ public/js/cart.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
       url: "/cart",
       data: { update: sku, quantity: qty, xsrf_token: xsrfMeta },
       success: function(json) {
-        el.closest("table").replaceWith(json.html);
+        el.closest("div.cart-fragment").replaceWith(json.html);
         updateCartQuantityDisplay();
       }
     });
