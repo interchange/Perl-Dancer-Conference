e28c482c5ef32575c79fcd4f04229e9e4ede6932
Sam Batschelet <sbatschelet@mac.com>
2015-07-25 16:32:37 -0400

    add bootstrap-datetimepicker plugin assets.

---
+++ public/js/bootstrap-datetimepicker.config.js
@@ -0,0 +1,9 @@
+// bootstrap-datetimepicker.min.js confiruration file
+// see https://github.com/smalot/bootstrap-datetimepicker
+
+$('.form_datetime').datetimepicker({
+    update: '1',
+    format: 'yyyy-mm-dd hh:ii',
+    startDate:   new Date(),
+});
+
