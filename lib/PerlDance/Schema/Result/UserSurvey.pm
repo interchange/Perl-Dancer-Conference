e178d8b5bc36ffa21ed98c4d91d7c763c05d0a18
Peter Mottram <peter@sysnix.com>
2015-10-29 18:55:50 +0100

    initial UserSurvey class

---
+++ lib/PerlDance/Schema/Result/UserSurvey.pm
@@ -0,0 +1,93 @@
+package PerlDance::Schema::Result::UserSurvey;
+
+=head1 NAME
+
+PerlDance::Schema::Result::UserSurvey
+
+=cut
+
+use Interchange6::Schema::Candy;
+
+=head1 ACCESSORS
+
+=head2 user_survey_id
+
+PK
+
+=cut
+
+primary_column user_survey_id =>
+  { data_type => "integer", is_auto_increment => 1 };
+
+=head2 users_id
+
+FK on L<Interchange6::Schema::Result::User/users_id>
+
+=cut
+
+column users_id => { data_type => "integer" };
+
+=head2 survey_id
+
+FK on L<PerlDance::Schema::Result::Survey/survey_id>
+
+=cut
+
+column survey_id => { data_type => "integer" };
+
+=head1 UNIQUE CONSTRAINT
+
+=head2 users_id_survey_id
+
+=over 4
+
+=item * L</users_id>
+
+=item * L</survey_id>
+
+=back
+
+=cut
+
+unique_constraint users_id_survey_id => [qw/users_id survey_id/];
+
+
+=head1 RELATIONS
+
+=head2 user
+
+Type: belongs_to
+
+Related object: L<Interchange6::Schema::Result::User>
+
+=cut
+
+belongs_to
+  user => 'Interchange6::Schema::Result::User',
+  'users_id';
+
+=head2 survey
+
+Type: belongs_to
+
+Related object: L<PerlDance::Schema::Result::Survey>
+
+=cut
+
+belongs_to
+  survey => 'PerlDance::Schema::Result::Survey',
+  'survey_id';
+
+=head2 responses
+
+Type: has_many
+
+Related object: L<PerlDance::Schema::Result::SurveyResponse>
+
+=cut
+
+has_many
+  responses => 'PerlDance::Schema::Result::SurveyResponse',
+  'user_survey_id';
+
+1;
