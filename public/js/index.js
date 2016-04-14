0433991699d4a37300dae30fc5d98e82e763555d
Stefan Hornburg (Racke) <racke@linuxia.de>
2016-04-14 11:03:46 +0200

    Remove Amerikahaus from map and frontpage.

--- public/js/index.js
+++ public/js/index.js
@@ -6,14 +6,8 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   id: 'petermottram.mp1dpak2',
   accessToken: 'pk.eyJ1IjoicGV0ZXJtb3R0cmFtIiwiYSI6IjE1NWI5NjRjN2IxNjNkYTM1MzI3YzY5M2E0YjZjMDc0In0.T0bpnZGXCQBSW_SOv_nWHA'
 }).addTo(map);
-var marker1 = L.marker([48.21008, 16.35578]).addTo(map);
 var marker2 = L.marker([48.18246, 16.38075]).addTo(map);
-marker1.bindPopup("<b>Amerikahaus</b><br>Friedrich-Schmidt-Platz 2<br>Training Venue<br>19/20 Oct");
 marker2.bindPopup("<b>Hotel Schani Wien</b><br>Karl-Popper-Straße 22<br>Conference Venue<br>21/22 Oct");
-$("#marker1").click(function(){
-    marker1.openPopup();
-    return false;
-});
 $("#marker2").click(function(){
     marker2.openPopup();
     return false;
