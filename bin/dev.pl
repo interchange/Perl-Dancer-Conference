d3135699f0e45eaa0e51d15ddf57a7027af8fcb3
Peter Mottram <peter@sysnix.com>
2016-04-20 18:50:18 +0200

    move dec.pl to D2 and switch to Plack::Debugger

--- bin/dev.pl
+++ bin/dev.pl
@@ -1,45 +1,102 @@
 #!/usr/bin/env perl
-#use Dancer;
-#use DanceShop;
-#dance;
+
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/../lib";
 
 use Plack::Builder;
-use Plack::Middleware::Debug::DBIC::QueryLog;
-use Plack::Middleware::DBIC::QueryLog;
-use Dancer;
-use Dancer::Plugin::DBIC;
-use Dancer::Handler;
-use lib 'lib';
+
+use JSON;
+ 
+use Plack::Debugger;
+use Plack::Debugger::Storage;
+ 
+use Plack::App::Debugger;
+ 
+use Plack::Debugger::Panel::AJAX;
+use Plack::Debugger::Panel::Memory;
+use Plack::Debugger::Panel::Parameters;
+use Plack::Debugger::Panel::PlackRequest;
+use Plack::Debugger::Panel::PlackResponse;
+use Plack::Debugger::Panel::Timer;
+use Plack::Debugger::Panel::Warnings;
+ 
 use PerlDance;
+my $app = PerlDance->to_app;
 
-my $app = sub {
-    load_app "PerlDance";
-    Dancer::App->set_running_app("PerlDance");
-    my $env = shift;
-    my $schema = schema->clone;
-    my $querylog =
-      Plack::Middleware::DBIC::QueryLog->get_querylog_from_env($env);
-    $schema->storage->debug(1);
-        $schema->storage->debugobj($querylog);
-    Dancer::Handler->init_request_headers($env);
-    my $req = Dancer::Request->new( env => $env );
-    Dancer->dance($req);
-};
-
+my $debugger = Plack::Debugger->new(
+    storage => Plack::Debugger::Storage->new(
+        data_dir     => '/tmp/debugger_panel',
+        serializer   => sub { encode_json( shift ) },
+        deserializer => sub { decode_json( shift ) },
+        filename_fmt => "%s.json",
+    ),
+    panels => [
+        Plack::Debugger::Panel::AJAX->new, 
+        Plack::Debugger::Panel::Memory->new,
+        Plack::Debugger::Panel::Parameters->new,
+        Plack::Debugger::Panel::PlackRequest->new,
+        Plack::Debugger::Panel::PlackResponse->new,
+        Plack::Debugger::Panel::Timer->new,     
+        Plack::Debugger::Panel::Warnings->new   
+    ]
+);
+ 
+my $debugger_app = Plack::App::Debugger->new( debugger => $debugger );
+ 
 builder {
     enable 'Session';
     enable 'XSRFBlock',
-      cookie_name => 'PerlDance-XSRF-Token',
-      meta_tag => 'xsrf-meta',
-      cookie_options => {
-          httponly => 1,
-      };
-    enable 'Debug',
-      panels => [
-        'Parameters',                'Dancer::Version',
-        'Dancer::Settings',          'Dancer::Logger',
-        'Dancer::TemplateVariables', 'DBIC::QueryLog',
-        'Timer',#                     'Profiler::NYTProf',
-      ];
-    $app;
+      cookie_name    => 'PerlDance-XSRF-Token',
+      meta_tag       => 'xsrf-meta',
+      cookie_options => { httponly => 1, };
+
+    mount $debugger_app->base_url => $debugger_app->to_app;
+
+    mount '/' => builder {
+        enable $debugger_app->make_injector_middleware;
+        enable $debugger->make_collector_middleware;
+        $app;
+    }
 };
+
+#use Plack::Middleware::Debug::DBIC::QueryLog;
+#use Plack::Middleware::DBIC::QueryLog;
+#use Dancer2;
+#use Dancer2::Plugin::DBIC;
+#use Dancer::Handler;
+#use lib 'lib';
+#use PerlDance;
+
+#my $app = sub {
+#    load_app "PerlDance";
+#    Dancer::App->set_running_app("PerlDance");
+#    my $env = shift;
+#    my $schema = schema->clone;
+#    my $querylog =
+#      Plack::Middleware::DBIC::QueryLog->get_querylog_from_env($env);
+#    $schema->storage->debug(1);
+#        $schema->storage->debugobj($querylog);
+#    Dancer::Handler->init_request_headers($env);
+#    my $req = Dancer::Request->new( env => $env );
+#    Dancer->dance($req);
+#};
+
+#builder {
+#    enable 'Session';
+#    enable 'XSRFBlock',
+#      cookie_name => 'PerlDance-XSRF-Token',
+#      meta_tag => 'xsrf-meta',
+#      cookie_options => {
+#          httponly => 1,
+#      };
+#    enable 'Debug',
+#      panels => [
+#        'Parameters',                'Dancer::Version',
+#        'Dancer::Settings',          'Dancer::Logger',
+#        'Dancer::TemplateVariables', 'DBIC::QueryLog',
+#        'Timer',#                     'Profiler::NYTProf',
+#      ];
+#    $app;
+#};
